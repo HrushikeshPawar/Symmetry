fe5473b4a5b297698289da3c18a00b2e7ade3c1b
HrushikeshPawar <hrushikeshpawar96@gmail.com>
2020-05-31 18:42:23 +0530

    Added The Summary of the "WHAT ?" part

--- Test.py
+++ Test.py
@@ -331,3 +331,129 @@ class Abstract(Scene):
         self.play(FadeOut(arrow5),Rotate(square2, PI/4))
         self.play(ShowCreation(Cross(inv)))
         self.wait(3)
+
+class Summary_What(Scene):
+    def construct(self):
+
+        def intro(object, pos):
+            self.play(scene.set_opacity, 0.1, FadeInFromPoint(object, ORIGIN))
+            self.wait()
+            self.play(object.scale, 0.3, object.shift, pos,
+            scene.set_opacity, 1)
+            scene.add(object)
+            self.wait()
+
+        def DottedLines(object, angle, lines):
+            line = Line(UP*2.5, DOWN*2.5)
+            dline = DashedVMobject(line)
+            dlines = [0]*(lines-1)
+            for i in range(lines-1):
+                dlines[i]= dline.copy().rotate(angle*(i+1))
+            group = VGroup(object,dline)
+            for x in dlines:
+                group.add(x)
+            return group
+
+        #Title of the Scene
+        title = TextMobject("Symmetry", "  -  ", "What", " ?").scale(1.5)
+        title[2].set_color(RED)
+        title.move_to(UP*3)
+
+        #Headings
+        heading1 = TexMobject("General \, Language", color=BLUE_D).scale(0.7)
+        heading2 = TexMobject("Geometry", color=BLUE_D).scale(0.7)
+        heading3 = TexMobject("Abstract", color=BLUE_D).scale(0.7)
+        heading1.move_to(UP*1.5 + LEFT*4.5)
+        heading2.move_to(UP*1.5)
+        heading3.move_to(UP*1.5 + RIGHT*4)
+        headings = VGroup(heading1, heading2, heading3)
+
+        #Create Scene with title and headings
+        self.wait()
+        self.play(Write(title))
+        self.play(FadeInFromDown(heading1), FadeInFromDown(heading2),
+        FadeInFromDown(heading3))
+        self.wait()
+
+        #A group for whole scene, which will be updated throughout the animation
+        scene = VGroup(title, headings)
+
+        #import required SVG's, Draw shapes and created dotted lines and
+        #play the animations
+
+        global svg_group
+        svg_group = []
+
+        ##1st SVG Trees##
+        trees = SVGMobject("My_Projects/Symmetry/SVG/Asymmetric.svg",
+        color=GREEN_D, fill_opacity=1, stroke_width=1).scale(2)
+
+        intro(trees, LEFT*4.5)
+        svg_group.append(trees)
+
+        ##2nd SVG - ButterFly##
+        fly = SVGMobject("My_Projects/Symmetry/SVG/BFly.svg",
+        color=GREEN_D, stroke_width=1, fill_opacity=1).scale(2)
+        svg_group.append(fly)
+        intro(fly, LEFT*4.5 + DOWN*2)
+
+        ##3rd SVG - star##
+        star = SVGMobject("My_Projects/Symmetry/SVG/Star.svg",
+        color=WHITE, stroke_width=1, fill_opacity=1).scale(2)
+        star_group = DottedLines(star, PI/6, 6)
+        intro(star_group, ORIGIN)
+
+        ##4th SVG - Hexa##
+        hexa = SVGMobject("My_Projects/Symmetry/SVG/Rotate.svg",
+        color=WHITE, stroke_width=1, fill_opacity=0).scale(2)
+        svg_group.append(hexa)
+        self.play(scene.set_opacity, 0.1, FadeInFromPoint(hexa, ORIGIN))
+        self.wait()
+        self.play(scene.set_opacity, 1, hexa.shift, DOWN*2, hexa.scale, 0.3)
+        scene.add(hexa)
+        self.wait(2)
+
+        #For Abstract Definition
+        self.play(scene.set_opacity, 0)
+        inv = TextMobject("Invariance").move_to(UP*3+RIGHT*5)
+        op1 = TextMobject("Operations").move_to(UP*3+ LEFT*5)
+        op2 = TextMobject("Transformation").move_to(UP*3)
+        self.add(op1)
+        self.add(op2)
+        self.add(inv)
+        self.wait()
+
+        square = Square(color=BLUE_D).move_to(LEFT*4)
+        arrow1 = Arrow(DOWN*2.5+LEFT*4,LEFT*4+DOWN*1.5)
+        text1 = TextMobject("Mathematical\\ Object").move_to(DOWN*3+LEFT*4).scale(0.5)
+        self.play(ShowCreation(square))
+        self.play(FadeInFromDown(arrow1), FadeInFromDown(text1))
+
+        arrow2 = Arrow(LEFT*2, ORIGIN)
+        text2 = TextMobject("Rotate").move_to(LEFT + UP*0.5).scale(0.7)
+        text3 = TexMobject(r"90^{\circ}").move_to(LEFT+DOWN*0.5).scale(0.7)
+        arrow3 = Arrow(LEFT + UP*1.5, LEFT+UP*0.5)
+        self.play(FadeIn(arrow2), FadeIn(text2),FadeIn(text3))
+        self.play(ShowCreation(arrow3),op1.scale, 0.5, op1.next_to, arrow3,UP)
+
+        square2 = square.copy().set_color(RED)
+        self.play(square2.shift, RIGHT*6)
+        self.play(op2.shift, UP*3)
+        op2 = TextMobject("Transformation").scale(0.5).move_to(RIGHT*2+DOWN*3)
+        arrow4 = Arrow(DOWN*2.5+RIGHT*2,DOWN*1.5+RIGHT*2)
+        self.play(FadeInFromDown(arrow4), FadeInFromDown(op2))
+        self.play(Rotate(square2, PI/2))
+
+        arrow5 = Arrow(RIGHT*4, RIGHT*3)
+        square3 = square.copy()
+        self.play(FadeIn(arrow5), inv.scale, 0.5, inv.next_to, arrow5,RIGHT,
+        square3.shift, RIGHT*6)
+
+        group = VGroup(arrow1, arrow2, arrow3, arrow4, arrow5, square, square2,
+        square3, text2, text3)
+
+        self.play(text1.move_to, text1.get_center()*0 +RIGHT*4, FadeOut(group),
+        op1.move_to, op1.get_center()*0+RIGHT*4+DOWN*0.7,
+        op2.move_to, op2.get_center()*0+RIGHT*4+DOWN*1.4,
+        inv.move_to, inv.get_center()*0+RIGHT*4+DOWN*2.1, scene.set_opacity, 1)
+        self.wait(3)
--- Testing.py
+++ Testing.py
@@ -2,11 +2,44 @@ from manimlib.imports import *
 
 class Test(Scene):
     def construct(self):
-        abst = TextMobject("''Symmetry is a type of ", "invariance",", a property of a mathematical objects remains unchanged, ","under a set of ","operations ","or ","transformations.''")
-        abst[1].set_color(RED)
-        abst[4].set_color(GREEN)
-        abst[6].set_color(GREEN)
-        print(str(abst[1].get_tex_string()))
-        cross = Cross(abst[1], stroke_width=0.1)
-        self.play(ShowCreation(cross))
+        inv = TextMobject("Inversion").scale(0.5).move_to(UP*3+RIGHT*5)
+        op1 = TextMobject("Operations").scale(0.5).move_to(UP*3+ LEFT*5)
+        op2 = TextMobject("Transformation").scale(0.5).move_to(UP*3 + LEFT)
+        self.add(op1)
+        self.add(op2)
+        self.add(inv)
         self.wait()
+
+        square = Square(color=BLUE_D).move_to(LEFT*4)
+        arrow1 = Arrow(DOWN*2.5+LEFT*4,LEFT*4+DOWN*1.5)
+        text1 = TextMobject("Mathematical\\ Object").move_to(DOWN*3+LEFT*4).scale(0.5)
+        self.play(ShowCreation(square))
+        self.play(FadeInFromDown(arrow1), FadeInFromDown(text1))
+
+        arrow2 = Arrow(LEFT*2, ORIGIN)
+        text2 = TextMobject("Rotate").move_to(LEFT + UP*0.5).scale(0.7)
+        text3 = TexMobject(r"90^{\circ}").move_to(LEFT+DOWN*0.5).scale(0.7)
+        arrow3 = Arrow(LEFT + UP*1.5, LEFT+UP*0.5)
+        self.play(FadeIn(arrow2), FadeIn(text2),FadeIn(text3))
+        self.play(ShowCreation(arrow3), op1.next_to, arrow3,UP)
+
+        square2 = square.copy().set_color(RED)
+        self.play(square2.shift, RIGHT*6)
+        self.play(op2.shift, UP*3)
+        op2 = TextMobject("Transformation").scale(0.7).move_to(RIGHT*2+DOWN*3)
+        arrow4 = Arrow(DOWN*2.5+RIGHT*2,DOWN*1.5+RIGHT*2)
+        self.play(FadeInFromDown(arrow4), FadeInFromDown(op2))
+        self.play(Rotate(square2, PI/2))
+
+        arrow5 = Arrow(RIGHT*4, RIGHT*3)
+        square3 = square.copy()
+        self.play(FadeIn(arrow5), inv.next_to, arrow5,RIGHT, square3.shift, RIGHT*6)
+
+        group = VGroup(arrow1, arrow2, arrow3, arrow4, arrow5, square, square2,
+        square3, text2, text3)
+
+        self.play(text1.move_to, text1.get_center()*0 +RIGHT*4+UP, FadeOut(group),
+        op1.move_to, op1.get_center()*0+RIGHT*4,
+        op2.move_to, op2.get_center()*0+RIGHT*4+DOWN,
+        inv.move_to, inv.get_center()*0+RIGHT*4+DOWN*2)
+        self.wait(3)
